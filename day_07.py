7b0a2c53aa8eb9681285f2e20ae9f988769c8f07
scottabelden <scottabelden@gmail.com>
2024-12-07 22:40:36 -0500

    day 7

---
+++ day_07.py
@@ -0,0 +1,59 @@
+from itertools import product
+from operator import add, mul
+from collections.abc import Callable
+
+from utils import answer1, answer2, get_input
+
+lines = get_input("day_07_input")
+
+
+def combine(a: int, b: int) -> int:
+    return int(str(a) + str(b))
+
+
+def test_args(args: list[int], operations: tuple[Callable[[int, int], int], ...]) -> int:
+    # print(f"testing {args=} and {operations=}")
+    total = 0
+    for index, arg in enumerate(args):
+        if index == 0:
+            total = arg
+            continue
+
+        total = operations[index - 1](total, arg)
+
+    # print(f"{total=}")
+    return total
+
+
+calibration = 0
+for line in lines:
+    total, rest = line.split(": ")
+    args = [int(item) for item in rest.split()]
+
+    combinations = product((add, mul), repeat=len(args) - 1)
+
+    for combination in combinations:
+        test_total = test_args(args, combination)
+        if test_total == int(total):
+            calibration += test_total
+            break
+
+
+answer1(calibration)
+
+
+calibration = 0
+for line in lines:
+    total, rest = line.split(": ")
+    args = [int(item) for item in rest.split()]
+
+    combinations = product((add, mul, combine), repeat=len(args) - 1)
+
+    for combination in combinations:
+        test_total = test_args(args, combination)
+        if test_total == int(total):
+            calibration += test_total
+            break
+
+
+answer2(calibration)
